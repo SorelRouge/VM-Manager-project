8822fa2f68e5b4dea854335c3736590303a54429
SorelRouge <kevin.astier88@gmail.com>
2019-08-13 12:56:34 +0200

    comment ok, date_insert ok

--- requetes.php
+++ requetes.php
@@ -101,14 +101,14 @@ function afficherTableau ($donnees, $attributs=array())
 function insertIntoDropdown()
 {
     $mysqli = Dbh::connexion();
-    $records = $mysqli->query("SELECT * FROM machines");
+    $records = $mysqli->query("SELECT * FROM virtualmachines");
     echo "<select name='machines' onchange=showMachine(this.value)>";
     echo "<option value=\"\">Select a machine</option>";
     while ($row = $records->fetch_assoc())
     {
         unset($name);
         $name = $row['name'];
-        $id = $row['id'];
+        $id = $row['vmid'];
         echo "<option value=\"$id\">" . $name . "</option>";
     }
     
@@ -151,5 +151,65 @@ function getMachines()
     }
 }
 
+
+/* requête d'insert dans la BDD à partir du fichier XML */
+function insertFromXml($vms)
+{ 
+    
+  foreach ($vms->CONTENT->children() as $virtual)
+  {
+    $mysqli = Dbh::connexion();
+    /* évite les conflits avec les quotes */
+    $comment = $mysqli->real_escape_string($virtual->COMMENT);
+    $name = $mysqli->real_escape_string($virtual->NAME);
+    $status = $mysqli->real_escape_string($virtual->STATUS);
+
+      /* utilisation */
+    $SQL = "INSERT INTO virtualmachines 
+    (comment, mac, memory, name, status, uuid, vcpu, vmid, vmtype, date_insert) 
+    VALUES (?,?,?,?,?,?,?,?,?, NOW()) 
+    ON DUPLICATE KEY UPDATE
+    comment='$comment',
+    mac='$virtual->MAC',
+    memory = $mysqli->quote($virtual->MEMORY),
+    name='$name',
+    status='$status',
+    uuid='$virtual->UUID',
+    vcpu=$mysqli->quote($virtual->VCPU),
+    vmid='$virtual->VMID',
+    vmtype='$virtual->VMTYPE'";
+    /* préparation */
+    $stmt=$mysqli->prepare($SQL);
+    /* bind variable à la préparation */
+    $stmt->bind_param('ssisssiss', 
+    $virtual->COMMENT, 
+    $virtual->MAC, 
+    $virtual->MEMORY, 
+    $virtual->NAME, 
+    $virtual->STATUS, 
+    $virtual->UUID, 
+    $virtual->VCPU, 
+    $virtual->VMID, 
+    $virtual->VMTYPE);
+    /* execution */
+    $ok = $stmt->execute ();
+    /* fermeture de l'ordre */
+    $stmt->close();
+    if($ok)
+    {
+        $mysqli->commit();
+    }
+  };
+};
+/* rnfverjnf 
+ name='$virtual->NAME',
+memory = $mysqli->quote($virtual->MEMORY),
+
+comment='$virtual->COMMENT',
+mac='$virtual->MAC'
+*/
+
+
 ?>
+
 
